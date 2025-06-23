6fb885ffca14650bbdb0df3d525776a833e812c8
Angel-enc <enock8876@gmail.com>
2025-06-23 10:30:39 +0000

    generador de dominio

--- src/app.js
+++ src/app.js
@@ -9,3 +9,18 @@ window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
 };
+let pronoun = ['the', 'our'];
+let adj = ['great', 'big'];
+let noun = ['jogger', 'racoon'];
+let domain = ['.com', '.es', '.org', '.net']
+
+for (let i = 0; i < pronoun.length; i++) {
+  for (let j = 0; j < adj.length; j++) {
+    for (let h = 0; h < noun.length; h++) {
+      for (let k = 0; k <domain.length; k++){
+      let elements = pronoun[i] + adj[j] + noun[h] + domain[k];
+      console.log(elements);
+      }
+    }
+  }
+}
